63ed3ce97ba5d79359029be115913fc833ddf25f
Dasha A <dareiios@mail.ru>
2026-02-02 21:41:49 +0300

    ordering domain

---
+++ src/Services/Ordering/Ordering.Domain/Abstraction/IAggregate.cs
@@ -0,0 +1,12 @@
+﻿namespace Ordering.Domain.Abstraction
+{
+    public interface IAggregate<T> : IAggregate, IEntity<T>
+    {
+    }
+
+    public interface IAggregate : IEntity
+    {
+        IReadOnlyList<IDomainEvent> DomainEvents { get; } //there will be IDomain event related objects
+        IDomainEvent[] ClearDomainEvents();
+    }
+}
---
+++ src/Services/Ordering/Ordering.Domain/Abstraction/IDomainEvent.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace Ordering.Domain.Abstraction
+{
+    // allows domain events to be dispatched through the mediatr handler
+    // so we use mediatr handlers in order to handle these events using mediatr
+    public interface IDomainEvent : INotification 
+    {
+        Guid EventId => Guid.NewGuid();
+        public DateTime OccurredOn => DateTime.Now;
+        public string EventType => GetType().AssemblyQualifiedName;//which class is throwing this event
+    }
+}
